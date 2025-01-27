c7d08834843d9fc588fa5bb94b45dd1064468222
Jaya DeHart <dehartj97@gmail.com>
2025-01-27 12:21:25 -0800

    CORS

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const port = 8000;
 async function start() {
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "https://jayadehart.com"],
     })
   );
   app.use(express.json());
